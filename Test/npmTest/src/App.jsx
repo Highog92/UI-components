c59c9af1676bc1f7306bff55787f4dbeb4435160
Highog92 <Madsserritslev@gmail.com>
2024-01-05 12:16:31 +0100

    Update

---
+++ Test/npmTest/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { Darkmode } from '@mads/ui-components'
+import { Buttons } from '@mads/ui-components'
+import { Input } from '@mads/ui-components'
+import { Nav } from '@mads/ui-components'
+import { Grid } from '@mads/ui-components'
+
+function App() {
+
+    return (
+      <>
+        <Darkmode />
+        <Buttons/>
+        <Input/>
+        <Nav/>
+        <Grid/>
+
+      </>
+    )
+  }
+
+export default App
--- UI_Components/src/Components/Button/Buttons.jsx
+++ UI_Components/src/Components/Button/Buttons.jsx
@@ -9,6 +9,30 @@ function UIbuttons() {
             <button className={buttons.button2}><img src={HouseSVG} />Button 2</button>
             <button className={buttons.button3}><img src={HouseSVG} />Button 3</button>
             <button className={buttons.button4}>Button 4</button>
+
+            <div class="container">
+                <a class="button" href="#" style="--color:#1e9bff;">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    Button
+                </a>
+                <a class="button" href="#" style="--color: #ff1867;">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    Button
+                </a>
+                <a class="button" href="#" style="--color: #6eff3e;">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    Button
+                </a>
+            </div>
         </>
     )
 }
